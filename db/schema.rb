71e02f9e349c77fb932cad72d7589509fa68fac6
Anders Janmyr <anders@janmyr.com>
2013-05-28 18:44:18 +0200

    Changed config to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130528084759) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: true do |t|
     t.string   "name"
     t.integer  "rate"
@@ -27,12 +30,12 @@ ActiveRecord::Schema.define(version: 20130528084759) do
     t.integer  "item"
     t.string   "table"
     t.integer  "month",      limit: 2
-    t.integer  "year",       limit: 5
+    t.integer  "year",       limit: 8
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories"
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories", using: :btree
 
   create_table "time_entries", force: true do |t|
     t.integer  "user_id"
@@ -44,8 +47,8 @@ ActiveRecord::Schema.define(version: 20130528084759) do
     t.datetime "updated_at"
   end
 
-  add_index "time_entries", ["project_id"], name: "index_time_entries_on_project_id"
-  add_index "time_entries", ["user_id"], name: "index_time_entries_on_user_id"
+  add_index "time_entries", ["project_id"], name: "index_time_entries_on_project_id", using: :btree
+  add_index "time_entries", ["user_id"], name: "index_time_entries_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -62,7 +65,7 @@ ActiveRecord::Schema.define(version: 20130528084759) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
