5ce1ebc98ba3b2b1c565c606f86b6be73a7e97c4
Anders Janmyr <anders@janmyr.com>
2013-05-28 09:40:31 +0200

    Generated rails_admin config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Times::Application.routes.draw do
   devise_for :users
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   get "welcome/index"
   root 'welcome#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130528072602) do
+ActiveRecord::Schema.define(version: 20130528073920) do
+
+  create_table "rails_admin_histories", force: true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      limit: 2
+    t.integer  "year",       limit: 5
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
