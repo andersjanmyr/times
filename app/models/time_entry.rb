4e5a3f2c4709d4b44cc09fa70a4773d3da972230
Anders Janmyr <anders@janmyr.com>
2013-05-28 16:35:06 +0200

    Added grouping function

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,4 +1,8 @@
 class TimeEntry < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+
+  def self.grouped_by_project_and_day
+    select('project_id, date(clocked_in) as date, sum(clocked_out - clocked_in) as total').group('project_id, date(clocked_in)')
+  end
 end
--- test/models/time_entry_test.rb
+++ test/models/time_entry_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class TimeEntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  describe '#grouped_by_project_and_day' do
+    before do
+      create_entry('Anders', 2.hours.ago, 1.hour.ago)
+      create_entry('Anders', 4.hours.ago, 2.hour.ago)
+      create_entry('Tapir', 2.hours.ago, 1.hour.ago)
+      @entries = TimeEntryTest.grouped_by_project_and_day
+    end
+
+    it 'contains two items' do
+      assert_equal(2, @entries.length)
+    end
+
+
+  end
+
+  private
+
+  def create_entry(project_name, clocked_in, clocked_out)
+    TimeEntry.create!({
+      project: Project.find_or_create_by(name: project_name),
+      clocked_in: clocked_in,
+      clocked_out: clocked_out
+    });
+  end
+
 end
