ef5346afd27f9f92e9bfd8fd765fdaf82055d3be
Anders Janmyr <anders@janmyr.com>
2013-05-28 13:41:21 +0200

    Added time_entries controller and fixed nested users routing.

---
+++ app/controllers/time_entries_controller.rb
@@ -0,0 +1,7 @@
+class TimeEntriesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Times::Application.routes.draw do
-  get "timesheet/index"
   devise_for :users
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
-  get "welcome/index"
+
+  get 'welcome/index'
   root 'welcome#index'
+
+  resources :users, path: '', only: [] do
+    get 'timesheet/index'
+    resources :time_entries, only: [:index, :create]
+  end
+
 end
