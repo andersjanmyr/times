7a12cc1e96bfbdf625dd87adffa857b93ad42621
Anders Janmyr <anders@janmyr.com>
2013-05-28 15:15:06 +0200

    Improved entry handling

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -7,18 +7,26 @@ class TimeEntriesController < ApplicationController
 
   def create
     entries = Times::TimeEntryParser.parse(params[:data])
-    time_entries = entries.map do |entry|
-      {
-        user_id: current_user.id,
-        project_id: Project.find_or_create_by(name: entry[:job]).id,
-        clocked_in: entry[:clocked_in],
-        clocked_out: entry[:clocked_out]
-      }
+    failed_entries = []
+    entries.each do |entry|
+      failed_entries << entry unless create_entry(entry)
     end
-    if TimeEntry.create(time_entries)
+
+    if failed_entries.empty?
       redirect_to timesheet_index_path, notice: "#{entries.length} entries uploaded"
     else
       format.html { render action: 'new' }
     end
+  end
+
+  private
+
+  def create_entry entry
+    TimeEntry.find_or_create_by({
+      user_id: current_user.id,
+      project_id: Project.find_or_create_by(name: entry[:job]).id,
+      clocked_in: entry[:clocked_in],
+      clocked_out: entry[:clocked_out]
+    })
   end
 end
