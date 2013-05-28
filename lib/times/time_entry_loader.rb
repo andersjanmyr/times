7d6f55980eaefe9de20b9022004b44bcd93b76dc
Anders Janmyr <anders@janmyr.com>
2013-05-28 11:55:20 +0200

    Started with time_entry_loader

---
+++ lib/times/time_entry_loader.rb
@@ -0,0 +1,34 @@
+require 'csv'
+
+module Times
+  class TimeEntryLoader
+
+
+    def self.load text
+
+    end
+
+    # Parses CSV files as exported by HoursTracker app.
+    # "Job","Clocked In","Clocked Out","Duration","Hourly Rate","Earnings","Comment","Tags"
+    # "Studentlitteratur","2013-05-22 08:24","2013-05-22 11:54","3,5","850,00","2 975,00","",""
+    def self.parse(data)
+      table = CSV.parse(data, parse_options)
+      puts table.headers
+      table.each.map do |row|
+        convert_fields(row.fields(:job, :clocked_in, :clocked_out))
+      end
+    end
+
+    def self.parse_options
+      {
+        headers: :first_row,
+        header_converters: :symbol
+      }
+    end
+
+    def self.convert_fields values
+      [values[0], DateTime.parse(values[1]), DateTime.parse(values[2])]
+    end
+  end
+end
+
