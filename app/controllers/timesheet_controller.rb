aa4f68b75a61e507011cf8819015cb612cceda29
Anders Janmyr <anders@janmyr.com>
2013-05-29 17:14:53 +0200

    Fixed the first draft of new table

--- app/controllers/timesheet_controller.rb
+++ app/controllers/timesheet_controller.rb
@@ -1,6 +1,16 @@
 class TimesheetController < ApplicationController
   before_action :authenticate_user!
+
   def index
-    @entries =TimeEntry.where(user: current_user).grouped_by_project_and_day
+    @project_entries = project_entries
+  end
+
+  private
+
+  def project_entries
+    groups = TimeEntry.where(user: current_user).grouped_by_project
+    groups.map do |group|
+      [Project.find(group[0]), group[1]]
+    end
   end
 end
